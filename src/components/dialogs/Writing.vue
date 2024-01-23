adb99405bb6410751bf820d615b033dca4bbf7a8
hanmincheol <hmc01100@gmail.com>
2024-01-23 14:18:58 +0900

    글 등록 ０１。２３ｖｅｒ

--- src/components/dialogs/Writing.vue
+++ src/components/dialogs/Writing.vue
@@ -1,12 +1,12 @@
 <script setup>
-import It from '@/pages/views/demos/components/tabs/ImageTab.vue'
 import Text from '@/pages/views/demos/forms/form-elements/textarea/WritingText.vue'
 import Sub from '@/views/demos/Subject.vue'
 import axios from '@axios'
 import { size } from '@floating-ui/dom'
 import avatar1 from '@images/avatars/avatar-1.png'
 import logo from '@images/logo.svg' // 로고 이미지 불러오기
-import { computed, ref } from 'vue'
+import bg from '@images/pages/writing.jpg'
+import { computed, reactive, ref, toRefs } from 'vue'
 
 const props = defineProps({
   isDialogVisible: {
@@ -16,19 +16,21 @@ const props = defineProps({
 })
 
 const emit = defineEmits(['update:isDialogVisible'])
-
-const imageUrls = ref([]) // 각 이미지의 URL을 저장하는 배열
-
 const isprofile = ref(false)
-
 const people = ref(false)
 const switchValue = computed(() => people.value ? 'Y' : 'N')
-
-
 const subValue = ref('카테고리')
 const textValue = ref('')
 const hashtagValue = ref('')
-const imageUrlsValue = ref([])
+let images = reactive({ files: [] })  // 선택된 이미지 파일을 저장하는 배열
+let activeTab = ref(0)
+let maxNameLength = 100  // 합쳐진 파일 이름의 최대 길이
+let dialog = ref(false)
+let message = ref('')  // 텍스트 박스의 데이터
+const { files } = toRefs(images)
+let fileInput = ref(null)  // 파일 입력을 위한 ref 생성
+let isLoading = ref(false)  // 로딩 상태를 나타내는 데이터 추가
+const ciu = ref([])
 
 const dialogVisibleUpdate = value => {
   emit('update:isDialogVisible', value)
@@ -36,7 +38,7 @@ const dialogVisibleUpdate = value => {
 
 
 const isButtonDisabled = computed(() => {
-  return subValue.value === '카테고리' || !textValue.value && !hashtagValue.value && imageUrlsValue.value.length === 0 
+  return subValue.value === '카테고리' || !textValue.value && !hashtagValue.value === 0 
 })
 
 const members = [
@@ -54,15 +56,15 @@ const submitData = async () => {
   formData.append('hashTag', hashtagValue.value)
   formData.append('type', subValue.value.value )
   formData.append('disclosureYN', switchValue.value)
-  console.log(hashtagValue.value)
+  formData.append('ciu', ciu.value)
+  console.log(ciu.value)
 
-  console.log(imageUrlsValue.value)
 
-  // 이미지 파일 추가
-  imageUrlsValue.value.forEach((file, index) => {
-    console.log(file)
-    formData.append(`file${index}`, file)
+  // 각 파일을 FormData에 추가
+  images.files.forEach(file => {
+    formData.append('files', file.file)  // 파일의 이름을 'files'로 지정
   })
+
 
   try {
     const response = await axios.post('http://localhost:4000/bbs/Write.do', formData, {
@@ -99,6 +101,86 @@ const removeHashtag = hashtag => {
     hashtags.value.splice(index, 1)
     hashtagValue.value = hashtags.value.join(' ')
   }
+}
+
+
+// 이미지 생성 요청 함수
+async function createImage() {
+  isLoading.value = true  // 요청 전에 로딩 상태를 true로 설정
+  try {
+    const response = await axios.post('http://localhost:5000/CreateIm', { message: message.value })
+
+    console.log(response.data.image_url)
+
+    images.files.unshift({ url: 'http://localhost:5000'+response.data.image_url, name: response.data.image_name })  // 이미지 정보를 객체로 저장
+
+    ciu.value.push({ url: 'http://localhost:5000'+response.data.image_url })
+    console.log(cimages.value)
+  } catch (e) {
+    console.error(e)
+  } finally {
+    isLoading.value = false  // 요청 완료 후에 로딩 상태를 false로 설정
+  }
+}
+
+// 파일 입력을 처리하는 함수
+const onFileChange = e => {
+  for (let file of e.target.files) {
+    images.files.push({ url: URL.createObjectURL(file), name: file.name, file: file })  // 이미지 정보를 객체로 저장
+  }
+  console.log(images.files[0].file)
+}
+
+const handleFileInput = files => {
+  for (let file of files) {
+    images.files.push({ url: URL.createObjectURL(file), name: file.name, file: file })  // 이미지 정보를 배열에 추가
+  }
+  console.log(images.files[0].file)
+  fileInput.value.reset()  // VFileInput 컴포넌트를 리셋
+}
+
+const sendMessage = () => {
+  createImage()   // 이미지 생성 요청
+}
+
+
+
+// 파일 이름을 가져오는 계산된 속성
+let fileNames = computed(() => {
+  let names = files.value.map(file => file.name)  // 이미지 정보에서 이름을 추출
+  let totalLength = names.join(', ').length
+  if (totalLength > maxNameLength) {
+    while (names.join(', ').length > maxNameLength) {
+      names[names.length - 1] = names[names.length - 1].slice(0, -1)
+    }
+    names[names.length - 1] += '...'  // 마지막 파일 이름에 줄임표 추가
+  }
+
+  return names.join(', ')
+})
+
+
+// 이미지 URL을 생성하는 함수
+const createImageUrl = image => {
+  return image.url  // 이미지 정보에서 URL을 반환
+}
+
+// 이미지를 제거하는 함수
+const removeImage = index => {
+  if (activeTab.value >= images.files.length) {
+    activeTab.value = images.files.length - 1
+  }
+  URL.revokeObjectURL(createImageUrl(images.files[index]))  // URL 해제를 먼저 수행
+  images.files.splice(index, 1)
+}
+
+
+const zoomIn = e => {
+  e.target.style.transform = 'scale(1.05)'
+}
+
+const zoomOut = e => {
+  e.target.style.transform = 'scale(1)'
 }
 </script>
 
@@ -224,10 +306,90 @@ const removeHashtag = hashtag => {
             </VCol>
             <VCol cols="6">
               <VCardItem>
-                <It
-                  v-model="imageUrlsValue"
-                  :image-urls="imageUrls"
-                />
+                <div>
+                  <VTextarea
+                    v-model="message"
+                    label="원하는 사진을 입력하세요"
+                    rows="2"
+                    no-resize
+                    style="margin-top: 10px;"
+                  />
+
+                  <VRow>
+                    <VCol cols="12">
+                      <VBtn
+                        block
+                        style="margin: 15px 0;"
+                        @click="sendMessage"
+                      >
+                        이미지 생성하기
+                      </VBtn>
+                    </VCol>
+                  </VRow>
+                  <div class="image-container">
+                    <img
+                      v-if="!isLoading"
+                      class="preview-image"
+                      :src="files.length > 0 ? createImageUrl(files[activeTab]) : bg"
+                      @click="dialog = true"
+                      @mouseover="zoomIn"
+                      @mouseout="zoomOut"
+                    >
+                    <VProgressCircular
+                      v-else
+                      class="loading"
+                      indeterminate
+                      color="primary"
+                    />
+                    <VBtn
+                      v-if="!isLoading && files.length > 0" 
+                      class="delete-button"
+                      icon
+                      small
+                      @click.stop="removeImage(activeTab)"
+                    >
+                      <VIcon>mdi-close-outline</VIcon>
+                    </VBtn>
+                  </div>
+                  <VDialog
+                    v-model="dialog"
+                    max-width="500px"
+                  >
+                    <VCard>
+                      <VImg :src="files.length > 0 ? createImageUrl(files[activeTab]) : bg" />
+                    </VCard>
+                  </VDialog>
+
+                  <VTabs
+                    v-model="activeTab"
+                    style="margin-bottom: 10px;"
+                  >
+                    <VTab
+                      v-for="(file, index) in files"
+                      :key="index"
+                      :value="index"
+                    >
+                      <VIcon icon="mdi-leaf-circle" />
+                    </VTab>
+                  </VTabs>
+
+                  <VFileInput
+                    ref="fileInput"
+                    multiple
+                    label="파일을 첨부하세요" 
+                    show-size
+                    prepend-icon="mdi-instagram"
+                    accept="image/*"
+                    @change="onFileChange"
+                    @file-input="handleFileInput"
+                  >
+                    <template #selection="{ text }">
+                      <div class="text-truncate">
+                        {{ fileNames }}
+                      </div>
+                    </template>
+                  </VFileInput>
+                </div>
               </VCardItem>
             </VCol>
           </VRow>
@@ -240,5 +402,34 @@ const removeHashtag = hashtag => {
 <style scoped>
 .pointer-cursor {
   cursor: pointer;
+}
+
+.image-container {
+  position: relative;
+  display: inline-block;
+  overflow: visible;  /* 이미지 크기 제한 풀기 */
+  border-style: solid;
+  block-size: 400px;
+  inline-size: 100%;
+}
+
+.preview-image {
+  block-size: 100%;
+  inline-size: 100%;
+  object-fit: cover;
+  transition: transform 0.5s ease-in-out; /* 이 부분을 추가하여 확대/축소 애니메이션을 부드럽게 만듬. */
+}
+
+.delete-button {
+  position: absolute;
+  inset-block-start: 0;
+  inset-inline-end: 0;
+}
+
+.loading {
+  position: absolute;
+  inset-block-start: 50%;
+  inset-inline-start: 50%;
+  transform: translate(-50%, -50%);
 }
 </style>
