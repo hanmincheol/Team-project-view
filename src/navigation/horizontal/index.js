3e3a6df003618d27ec7e1e8f2f9b4e341830033b
LeeSeungHwan <dltmdghks104@naver.com>
2024-01-28 20:20:41 +0900

    horizontal 수정

--- src/navigation/horizontal/index.js
+++ src/navigation/horizontal/index.js
@@ -1,34 +1,8 @@
+import community from "./community"
+import diary from "./diary"
+import diet from "./diet"
+import exercise from "./exercise"
+import myPage from "./myPage"
+import selfTest from "./selfTest"
 
-export default [
-  {
-    title: '식단',
-    to: { name: 'first-page' },
-    icon: { icon: 'mdi-food-fork-drink' },
-  },
-  {
-    title: '커뮤니티',
-    icon: { icon: 'mdi-account-group-outline' },
-    children: [
-      { title: '친구 / 구독자 관리 페이지', to: { name: 'community-user-tab', params: { tab: 'friend' } } },
-      { title: '챌린지목록', to: { name: 'apps-challengeList' }, icon: { icon: 'mdi-message-outline'  } },
-      { title: '채팅', to: { name: 'apps-chat' }, icon: { icon: 'mdi-message-outline'  } },
-      { title: '챌린지', to: { name: 'apps-user-id', params: { id: 21 } }, icon: { icon: 'mdi-crowd'  } },
-      { title: '게시판', to: { name: 'community_post' }, icon: { icon: 'mdi-instagram'  } },
-    ],
-  },
-  {
-    title: '마이페이지',
-    icon: { icon: 'mdi-face-man-shimmer-outline' },
-    to: { name: 'apps-user-view-id', params: { id: 22 }  },
-  },
-  {
-    title: '다이어리',
-    to: { name: 'fifth-page' },
-    icon: { icon: 'mdi-notebook' },
-  },
-  {
-    title: '메이트',
-    to: { name: 'mate' },
-    icon: { icon: 'mdi-notebook' },
-  },
-]
+export default [...myPage, ...community, ...selfTest, ...exercise, ...diet, ...diary]
---
+++ src/navigation/horizontal/selfTest.js
@@ -0,0 +1,8 @@
+
+export default [
+  {
+    title: '자가진단',
+    to: { name: 'selfTest' },
+    icon: { icon: 'mdi-account-check-outline' },
+  },
+]
