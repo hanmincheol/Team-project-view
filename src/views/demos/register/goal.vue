63e688e40ec1c1962ed4f070e47e56b4cd44cbc8
hanmincheol <hmc01100@gmail.com>
2024-02-20 23:55:54 +0900

    챌린지 리스트 거의 완성

---
+++ src/views/demos/register/goal.vue
@@ -0,0 +1,52 @@
+<template>
+  <VSelect
+    id="goal_No"
+    v-model="selectedOption"
+    :items="goal_No"
+    item-title="state"
+    item-value="goal_No"
+    persistent-hint
+    return-object
+    single-line
+    @change="handleSelect"
+  />
+</template>
+
+
+<script setup>
+import { defineEmits, ref } from 'vue'
+
+const emit = defineEmits('update:modelValue', goal_No)
+
+const selectedOption = ref("목표 설정")
+
+const goal_No = ref([
+  { state: '건강 관리', value: '건강 관리' },
+  { state: '체중 감량', value: '체중 감량' },
+  { state: '체중 증가', value: '체중 증가' },
+  { state: '근육 강화', value: '근육 강화' },
+  { state: '다이어트 식단', value: '다이어트 식단' },
+])
+
+
+function handleSelect(value) {
+  //var gender = document.querySelector('[name=gender]')
+
+  // gender.value = value ? 'M' : 'W'
+  // console.log('handleSelectedRadios호출:', gender.value)
+  emit('update:modelValue', value)  // gender의 값을 부모 컴포넌트로 넘김
+  console.log(newGoalNo)
+}
+
+const handleGoalNoChanged = newGoalNo => {
+  console.log(newGoalNo)
+}
+</script>
+
+
+
+<style>
+.Select {
+  padding-block-end: 50px;
+}
+</style>
