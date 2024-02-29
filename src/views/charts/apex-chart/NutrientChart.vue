90377a69e20046c5f2d859c3ee44938c422f9e46
osh <ostraderos@gmail.com>
2024-02-29 13:12:42 +0900

    식단 기록하기 - 사진 & 차트 연결

--- src/views/charts/apex-chart/NutrientChart.vue
+++ src/views/charts/apex-chart/NutrientChart.vue
@@ -1,23 +1,32 @@
 <script lang="ts" setup>
-import { getRadarChartConfig } from '@core/libs/apex-chart/lsyApexCharConfig';
-import { computed } from 'vue';
-import VueApexCharts from 'vue3-apexcharts';
-import { useTheme } from 'vuetify';
+import { getRadarChartConfig } from '@core/libs/apex-chart/lsyApexCharConfig'
+import { computed } from 'vue'
+import VueApexCharts from 'vue3-apexcharts'
+import { useTheme } from 'vuetify'
+
+const props = defineProps({
+  bfood: {
+    type: Array,
+    required: true,
+  },
+})
 
 const vuetifyTheme = useTheme()
 
-//오늘의 날짜 구하기
-var today = new Date();
-var year = today.getFullYear();
-var month = ('0' + (today.getMonth() + 1)).slice(-2);
-var day = ('0' + today.getDate()).slice(-2);
-var dateString = year + '-' + month  + '-' + day;
 
+//오늘의 날짜 구하기
+var today = new Date()
+var year = today.getFullYear()
+var month = ('0' + (today.getMonth() + 1)).slice(-2)
+var day = ('0' + today.getDate()).slice(-2)
+var dateString = year + '-' + month  + '-' + day
 
 const series = [
   {
     name: dateString,
-    data: [65, 46, 42, 25, 58],
+
+    // data: [65, 46, 42, 25, 58],
+    data: [props.bfood['carbohydrate'], props.bfood['protein'], props.bfood['fat'], props.bfood['sodium']/1000, props.bfood['cholesterol']/1000],
   },
 ]
 
